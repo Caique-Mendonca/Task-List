13f86349bcd4a6377fad3b27e5a2ec41fb5e4acc
Caique-Mendonca <caique4874@gmail.com>
2024-03-16 23:03:45 -0300

    Tentando selecionar e remover sprr

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let botao = document.querySelector('#botao-adicionar')
-botao.addEventListener('click', ()=>{
-    let inputTarefa = document.querySelector('#input').value
-    let tarefasOl = document.querySelector('.tarefas')
-    let tarefaLista = document.createElement('li')
-    tarefaLista.textContent = inputTarefa
-    tarefasOl.appendChild(tarefaLista)
-    document.querySelector('#input').value = ''
+window.addEventListener('load',()=>{
+    let botao = document.querySelector('#botao-adicionar')
+    botao.addEventListener('click', adicionarTarefa)
+    
+    function adicionarTarefa(){
+        let inputTarefa = document.querySelector('#input').value
+        let tarefasOl = document.querySelector('.tarefas')
+        let tarefaLista = document.createElement('li')
+        let checkbox = document.createElement('input')
+        let botaoDeletar = document.createElement('button')
+        let divDaLi = document.createElement('div')
+        botaoDeletar.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
+        botaoDeletar.classList.add('botao-remover')
+        tarefaLista.textContent = inputTarefa 
+        checkbox.type = "checkbox"
+        checkbox.classList.add('checkbox')
+        tarefasOl.appendChild(tarefaLista)
+        tarefaLista.appendChild(divDaLi)
+        divDaLi.appendChild(checkbox)
+        divDaLi.appendChild(botaoDeletar)
+        document.querySelector('#input').value = ''
+
+        botaoDeletar.addEventListener('click', ()=>{
+            document.querySelector('.tarefas li').style.display = "none"
+        })
+    }
+
 })
