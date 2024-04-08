d8c3086a22a952de2951b10a6fc4baa55530c890
caique-mendonca <caique4874@gmail.com>
2024-04-08 19:29:01 -0300

    .

--- script.js
+++ script.js
@@ -9,15 +9,24 @@ window.addEventListener('load',()=>{
         let checkbox = document.createElement('input')
         let botaoDeletar = document.createElement('button')
         let divDaLi = document.createElement('div')
+
         botaoDeletar.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
         botaoDeletar.classList.add('botao-remover')
+
+        tarefaLista.classList.add('tarefa-li')
         tarefaLista.textContent = inputTarefa 
+        tarefaLista.innerHTML
+
         checkbox.type = "checkbox"
         checkbox.classList.add('checkbox')
+
         tarefasOl.appendChild(tarefaLista)
+
         tarefaLista.appendChild(divDaLi)
+
         divDaLi.appendChild(checkbox)
         divDaLi.appendChild(botaoDeletar)
+
         document.querySelector('#input').value = ''
 
         botaoDeletar.addEventListener('click', ()=>{
