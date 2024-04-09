010aefe8b4f04ad6b71f36f65b8fbd1f5f799837
caique-mendonca <caique4874@gmail.com>
2024-04-08 21:29:57 -0300

    deletando as tarefas

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
+let contador = 0
+function deletarTarefa(id) {
+    let tarefaHtml = document.querySelector(`#${id}`)
+    tarefaHtml.remove()
+}
 window.addEventListener('load',()=>{
     let botao = document.querySelector('#botao-adicionar')
     botao.addEventListener('click', adicionarTarefa)
     
+
     function adicionarTarefa(){
         let inputTarefa = document.querySelector('#input').value
         let tarefasOl = document.querySelector('.tarefas')
         let tarefaLista = document.createElement('li')
-        let checkbox = document.createElement('input')
         let botaoDeletar = document.createElement('button')
-        let divDaLi = document.createElement('div')
-
-        botaoDeletar.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
-        botaoDeletar.classList.add('botao-remover')
-
         tarefaLista.classList.add('tarefa-li')
-        tarefaLista.textContent = inputTarefa 
-        tarefaLista.innerHTML
-
-        checkbox.type = "checkbox"
-        checkbox.classList.add('checkbox')
-
+        tarefaLista.innerHTML += `${inputTarefa} 
+        <div>
+            <input type="checkbox" class="checkbox">
+            <button class="botao-remover" onclick="deletarTarefa('${'tarefa-'+contador}')">
+                    <i class='fa-solid fa-trash-can'></i>
+                </button>
+        </div>` 
+        tarefaLista.id = `tarefa-${contador}`
+        contador++  
         tarefasOl.appendChild(tarefaLista)
-
-        tarefaLista.appendChild(divDaLi)
-
-        divDaLi.appendChild(checkbox)
-        divDaLi.appendChild(botaoDeletar)
 
         document.querySelector('#input').value = ''
 
@@ -33,5 +31,7 @@ window.addEventListener('load',()=>{
             document.querySelector('.tarefas li').style.display = "none"
         })
     }
+
+    
 
 })
