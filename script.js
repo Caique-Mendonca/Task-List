acdf51f7ae0d0a2a8bee44997b914e2262820eba
caique-mendonca <caique4874@gmail.com>
2024-04-08 22:10:21 -0300

    tratamento de erro campo vazio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let contador = 0
+let contador = 1
 function deletarTarefa(id) {
     let tarefaHtml = document.querySelector(`#${id}`)
     tarefaHtml.remove()
@@ -9,27 +9,38 @@ window.addEventListener('load',()=>{
     
 
     function adicionarTarefa(){
-        let inputTarefa = document.querySelector('#input').value
-        let tarefasOl = document.querySelector('.tarefas')
-        let tarefaLista = document.createElement('li')
-        let botaoDeletar = document.createElement('button')
-        tarefaLista.classList.add('tarefa-li')
-        tarefaLista.innerHTML += `${inputTarefa} 
-        <div>
-            <input type="checkbox" class="checkbox">
-            <button class="botao-remover" onclick="deletarTarefa('${'tarefa-'+contador}')">
-                    <i class='fa-solid fa-trash-can'></i>
-                </button>
-        </div>` 
-        tarefaLista.id = `tarefa-${contador}`
-        contador++  
-        tarefasOl.appendChild(tarefaLista)
+        try {
+            let inputTarefa = document.querySelector('#input').value
+            let tarefasOl = document.querySelector('.tarefas')
+            let tarefaLista = document.createElement('li')
+            let botaoDeletar = document.createElement('button')
+            let mensagemErro = document.querySelector('#error-message')
+            mensagemErro.innerText = ""
 
-        document.querySelector('#input').value = ''
+            if (inputTarefa == "") {
+                throw "O campo não pode estar vazio"
+            }
+            tarefaLista.classList.add('tarefa-li')
+            tarefaLista.innerHTML += `${inputTarefa} 
+            <div>
+                <input type="checkbox" class="checkbox">
+                <button class="botao-remover" onclick="deletarTarefa('${'tarefa-'+contador}')">
+                        <i class='fa-solid fa-trash-can'></i>
+                    </button>
+            </div>` 
+            tarefaLista.id = `tarefa-${contador}`
+            contador++  
+            tarefasOl.appendChild(tarefaLista)
+    
+            document.querySelector('#input').value = ''
+            botaoDeletar.addEventListener('click', ()=>{
+                document.querySelector('.tarefas li').style.display = "none"
+            })
+        } catch (error) {
+            let mensagemErro = document.querySelector('#error-message')
+            mensagemErro.innerText = error
+        }
 
-        botaoDeletar.addEventListener('click', ()=>{
-            document.querySelector('.tarefas li').style.display = "none"
-        })
     }
 
     
